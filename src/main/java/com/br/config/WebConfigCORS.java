db4cb59c34b46e2031c3fcde3bc4054a232ac497
Thiago Hernandes de Souza <hernandes_sp@yahoo.com.br>
2018-03-03 11:44:51 -0300

    Configuração de CORS para acesso de aplicações

---
+++ src/main/java/com/br/config/WebConfigCORS.java
@@ -0,0 +1,26 @@
+package com.br.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/** 
+ * Habilitando CORS para acesso de aplicações 
+ * @author Thiago Hernandes de Souza
+ * @since 03-03-2018
+ * */
+
+@Configuration
+@EnableWebMvc
+public class WebConfigCORS extends WebMvcConfigurerAdapter {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	registry.addMapping("/api/**")
+	.allowedOrigins("http://localhost:4200")
+	.allowedMethods("*")
+	.allowedHeaders("*")
+	.allowCredentials(false)
+	.maxAge(3600);
+	}
+}
